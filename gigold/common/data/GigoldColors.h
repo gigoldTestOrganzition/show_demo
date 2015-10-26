9487767c9dafafc6768d2333b01d91d716cb2aee
shanxiaoping-github <shanxiaoping@danxiaopingdeMacBook-Air.local>
2015-10-26 14:04:06 +0800

    颜色字体

---
+++ gigold/common/data/GigoldColors.h
@@ -0,0 +1,32 @@
+//
+//  GigoldColors.h
+//  gigold
+//
+//  Created by 单小萍 on 15/10/26.
+//  Copyright © 2015年 wsc. All rights reserved.
+//
+
+#ifndef GigoldColors_h
+#define GigoldColors_h
+#import "Colors.h"
+//主题色 4ACAE2
+#define theme_color kColorWithRGB(74,202,226,1)
+//白底辅助文字 A0A0A0
+#define white_auxiliary_color kColorWithRGB(160,160,160,1)
+//主题辅助色，按钮触摸颜色 80DAEA
+#define theme_auxiliary_color kColorWithRGB(128,218,234,1)
+//输入框提示颜色 D6D6D6
+#define input_hint_color kColorWithRGB(214,214,214,1)
+//背景色 F5F5F5
+#define back_ground_color kColorWithRGB(245,245,245,1)
+//灰底分割线 C7C7C7
+#define gray_diver_color kColorWithRGB(199,199,199,1)
+//主体文字颜色 595959
+#define main_text_color kColorWithRGB(89，89，89,1)
+//白底分割线 E5E5E5
+#define white_background_diver_color kColorWithRGB(229,229,229,1)
+//标题栏按钮上的偏白色文字 FFFFFF
+#define title_text_color kColorWithRGB(255,255,255,1)
+//按钮不可点击状态 CECECE
+#define unable_tap_color kColorWithRGB(206,206,206,1)
+#endif
